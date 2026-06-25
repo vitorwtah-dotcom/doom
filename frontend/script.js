5f25be6bb205e3c5961ec0a047b8597f1366f263
Vitor Walace Amancio Cardoso de Souza <1115220@senacminas.edu.br>
2026-06-25 16:51:50 -0300

    v0.7

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://doom-2-gm1h.onrender.com";
+const API = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" ? "http://localhost:3000" : "https://doom-1-jlhr.onrender.com";
 
 async function cadastrarAluno(event) {
 
